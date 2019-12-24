ccac6eea142d1b1b5cfa1a8b3b18589a6423d6df
Adefemi Kolawole <akolawole@ggc.edu>
2019-12-24 10:33:47 -0500

    Use ModelAndView Class in place of HTtpSession and HttpRequest to persist data from from to back

--- src/main/java/com/hiscox/miiapp_1/HomeController.java
+++ src/main/java/com/hiscox/miiapp_1/HomeController.java
@@ -2,7 +2,9 @@ package com.hiscox.miiapp_1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,18 +13,22 @@ import javax.servlet.http.HttpSession;
 public class HomeController {
 
     @RequestMapping("home")
+    //we are going to pass both the model and view within the controller class to fulfill MVC pattern.
     //@ResponseBody() // to create a string literal to be printed on page directly instead of executing an html page
-    public String home(HttpServletRequest req){
-        HttpSession session = req.getSession();
-        String name = req.getParameter("name");
+    public ModelAndView home(@RequestParam("name") String name, HttpSession session){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name, ");
+        mv.setViewName("home");
         System.out.println("Test: Hello servlet " + name);
-        session.setAttribute("name", name);
-        return "home";
+       // session.setAttribute("name", name);
+        //return "home";
+        return mv;
     }
 
     @RequestMapping("*")
     @ResponseBody() // to create a string literal to be printed on page directly instead of executing an html page
     public String error(){
+
         return "404: ERROR://Incorrect context path specified.";
     }
 
