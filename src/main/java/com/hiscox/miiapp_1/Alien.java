160b23571e0ebe876340eacd27218dd5f385c522
Adefemi Kolawole <akolawole@ggc.edu>
2019-12-24 04:24:10 -0500

    dependency injection, specifying whether you want singleton pattern or prototype in your design

---
+++ src/main/java/com/hiscox/miiapp_1/Alien.java
@@ -0,0 +1,40 @@
+package com.hiscox.miiapp_1;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype")//by default singleton design suggest create onl one instance of the object irrespective of how many times you instanciate the class. Prototype says, create according to the amount of instanciation
+public class Alien {
+    private String aid;
+    private String aName;
+    private String tech;
+
+    public Alien() {
+        System.out.println("ALien object created");
+    }
+
+    public String getAid() {
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+}
--- src/main/java/com/hiscox/miiapp_1/Miiapp1Application.java
+++ src/main/java/com/hiscox/miiapp_1/Miiapp1Application.java
@@ -2,12 +2,17 @@ package com.hiscox.miiapp_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Miiapp1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Miiapp1Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Miiapp1Application.class, args);
+
+       Alien a = context.getBean(Alien.class);
+       Alien b = context.getBean(Alien.class);
+
     }
 
 }
