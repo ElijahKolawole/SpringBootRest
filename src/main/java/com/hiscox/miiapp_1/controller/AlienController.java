9b69b538a427caba857a5d12e25cf1a63fab0474
Adefemi Kolawole <akolawole@ggc.edu>
2019-12-26 05:06:29 -0500

    use RestContoller instead of Contoller so that you can remove all @ResponseBody annotation. Also use PostMapping to write post method implementation

--- src/main/java/com/hiscox/miiapp_1/controller/AlienController.java
+++ src/main/java/com/hiscox/miiapp_1/controller/AlienController.java
@@ -4,17 +4,14 @@ import com.hiscox.miiapp_1.dao.AlienRepo;
 import com.hiscox.miiapp_1.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class AlienController {
     @Autowired
     AlienRepo repo;
@@ -44,14 +41,20 @@ public class AlienController {
     //for REST or web services
     //@RequestMapping(path="/aliens", produces = {"application/xml"})
     @RequestMapping("/aliens")
-    @ResponseBody()//does not look for any view. Simply return data as a web resource
+   // @ResponseBody()//does not look for any view. Simply return data as a web resource
     public List<Alien> returnAliens(){
         return repo.findAll();
     }
 
     @RequestMapping("/alien/{aid}")
-    @ResponseBody()//does not look for any view. Simply return data as a web resource
+    //@ResponseBody()//does not look for any view. Simply return data as a web resource
     public Optional<Alien> returnAlien(@PathVariable("aid") int aid){
+        return repo.findById(aid);
+    }
+
+    @PostMapping("/alien/{aid}")
+   // @ResponseBody()//does not look for any view. Simply return data as a web resource
+    public Optional<Alien> receiveAlien(@PathVariable("aid") int aid){
         return repo.findById(aid);
     }
 
